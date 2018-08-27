7332edf56d286d5fee19651b9c8b6ff260b9115b
yinzh <huisiwarmhome@163.com>
2018-08-27 15:33:40 +0800

    the first commit by netty design

---
+++ src/main/java/com/yin/netty/NettyDesignApplication.java
@@ -0,0 +1,12 @@
+package com.yin.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyDesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyDesignApplication.class, args);
+	}
+}
